8a5a3f2c22cb32e1c7511212d8063b4a2eb0aa42
YiubunAuyeung <549088764@qq.com>
2020-07-18 21:09:43 +0800

    add utility file

--- WRayEngine/WRayEngine/Application.h
+++ WRayEngine/WRayEngine/Application.h
@@ -17,6 +17,20 @@
 
 #include "glslang/SPIRV/GlslangToSpv.h"
 
+/* Number of descriptor sets needs to be the same at alloc,       */
+/* pipeline layout creation, and descriptor set layout creation   */
+#define NUM_DESCRIPTOR_SETS 1
+
+/* Number of samples needs to be the same at image creation,      */
+/* renderpass creation and pipeline creation.                     */
+#define NUM_SAMPLES VK_SAMPLE_COUNT_1_BIT
+
+/* Number of viewports and number of scissors have to be the same */
+/* at pipeline creation and in any call to set them dynamically   */
+/* They also have to be the same as each other                    */
+#define NUM_VIEWPORTS 1
+#define NUM_SCISSORS NUM_VIEWPORTS
+
 #define APP_SHORT_NAME "WRayEngine"
 #define U_ASSERT_ONLY
 
